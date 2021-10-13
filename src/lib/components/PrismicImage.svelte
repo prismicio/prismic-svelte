0033cb515f2444a5f45040d28a27bf05ac7e0dd0
Alex Trost <19617280+a-trost@users.noreply.github.com>
2021-10-13 19:18:20 -0400

    feat: Setup Prismicimage component

--- src/lib/components/PrismicImage.svelte
+++ src/lib/components/PrismicImage.svelte
@@ -1,1 +1,32 @@
-<img src="todo" alt="todo" />
+<script lang="ts">
+	import * as prismicT from "@prismicio/types";
+	export let field: prismicT.ImageField;
+	let className: string;
+
+	// creates a `class` property, even
+	// though it is a reserved word
+	export { className as class };
+
+	const usePrismicImage = (
+		field: prismicT.ImageField,
+	): {
+		url: string;
+		width?: number;
+		height?: number;
+		alt?: string;
+		copyright: string | undefined;
+	} => {
+		field.url;
+		return {
+			url: field.url || "",
+			alt: field.alt || "",
+			width: field.dimensions?.width || undefined,
+			height: field.dimensions?.height || undefined,
+			copyright: field.copyright || undefined,
+		};
+	};
+
+	const image = usePrismicImage(field);
+</script>
+
+<img src={image.url} alt={image.alt} height={image.height} />
---
+++ src/routes/components/image.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { PrismicImage } from "$lib";
+
+	const fields = {
+		full: {
+			url: "https://via.placeholder.com/150",
+			dimensions: {
+				width: 100,
+				height: 100,
+			},
+			alt: "Hey",
+			copyright: "Mine",
+		},
+		light: {
+			url: "https://via.placeholder.com/150",
+			dimensions: {
+				width: 100,
+				height: 100,
+			},
+		},
+	};
+</script>
+
+<PrismicImage field={fields.full} />
+<PrismicImage field={fields.light} />
