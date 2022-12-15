80f132b5a6ced21da9a7fdbe481c041a58971430
Sam Littlefair <sam.littlefair@prismic.io>
2022-12-15 10:14:56 +0000

    refactor: refactor Embed and Image components

--- src/lib/components/PrismicImage.svelte
+++ src/lib/components/PrismicImage.svelte
@@ -1,34 +1,22 @@
 <script lang="ts">
-	import * as prismicT from "@prismicio/types";
+	import * as prismicH from "@prismicio/helpers"
+	import type * as prismicT from "@prismicio/types";
+	
 	export let field: prismicT.ImageField;
+	
 	let className: string;
 
 	// creates a `class` property, even
 	// though it is a reserved word
 	export { className as class };
-
-	const usePrismicImage = (
-		field: prismicT.ImageField,
-	): {
-		url: string;
-		width?: number;
-		height?: number;
-		alt?: string;
-		copyright: string | undefined;
-	} => {
-		field.url;
-		return {
-			url: field.url || "",
-			alt: field.alt || "",
-			width: field.dimensions?.width || undefined,
-			height: field.dimensions?.height || undefined,
-			copyright: field.copyright || undefined,
-		};
-	};
-
-	const image = usePrismicImage(field);
 </script>
 
-{#if field.url}
-	<img src={image.url} alt={image.alt} height={image.height} />
+{#if prismicH.isFilled.image(field)}
+	{@const { alt, dimensions } = field}
+	{@const { width, height } = dimensions }
+	{@const { src, srcset } = prismicH.asImageWidthSrcSet(field)}
+	
+	<img {src} {srcset} {alt} {width} {height} class={className} />
+{:else}
+	<img alt={field.alt || ""} class={className} />
 {/if}
