dc4eb26872357fd0bd99f50abe28e89f35d91928
Sam Littlefair <sam.littlefair@prismic.io>
2023-02-23 14:52:28 +0000

    chore: git cleaning

--- src/lib/components/PrismicImage.svelte
+++ src/lib/components/PrismicImage.svelte
@@ -1,11 +1,4 @@
 <script lang="ts">
-<<<<<<< HEAD
-	import * as prismicH from "@prismicio/helpers"
-	import type * as prismicT from "@prismicio/types";
-	
-	export let field: prismicT.ImageField;
-	
-=======
 	import * as prismicH from "@prismicio/helpers";
 	import type * as prismicT from "@prismicio/types";
 
@@ -13,34 +6,15 @@
 
 	let className: string;
 
->>>>>>> master
-	// creates a `class` property, even
-	// though it is a reserved word
-	let className: string;
 	export { className as class };
-<<<<<<< HEAD
-
-	if(!prismicH.isFilled.image(field)) {
-		console.warn("Invalid prop")
-	}
-=======
->>>>>>> master
 </script>
 
 {#if prismicH.isFilled.image(field)}
 	{@const { alt, dimensions } = field}
-<<<<<<< HEAD
-	{@const { width, height } = dimensions }
-=======
 	{@const { width, height } = dimensions}
->>>>>>> master
 	{@const { src, srcset } = prismicH.asImageWidthSrcSet(field)}
 
 	<img {src} {srcset} {alt} {width} {height} class={className} />
 {:else}
 	<img alt={field.alt || ""} class={className} />
-<<<<<<< HEAD
 {/if}
-=======
-{/if}
->>>>>>> master
