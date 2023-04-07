aa6a13d6f28152cb8013225639a25d92009dbb39
Sam Littlefair <sam.littlefair@prismic.io>
2023-04-07 15:06:58 +0100

    refactor: remove unnecessary types

--- src/types/PrismicContext.type.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Client } from "@prismicio/client";
-import type { LinkResolverFunction, HTMLFunctionSerializer, HTMLMapSerializer } from "@prismicio/helpers";
-export type PrismicContext = {
-    client?: Client;
-    linkResolver?: LinkResolverFunction;
-    htmlSerializer?: HTMLFunctionSerializer | HTMLMapSerializer;
-};
