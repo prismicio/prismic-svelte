2756b909780c670e1dc1f8c4757d7c416bc952e8
Sam Littlefair <sam.littlefair@prismic.io>
2023-04-14 15:07:28 +0100

    chore: remove jsdoc comment and comment out unit tests

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,6 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
-/**
- * @type {import("@sveltejs/kit").Config}
- */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
