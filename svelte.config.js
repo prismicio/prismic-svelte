a912d6cd43fdc4ba461d46636e96c8953f1510cf
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-08-24 17:47:28 -1000

    chore: ignore linter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+// eslint-disable-next-line tsdoc/syntax
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
