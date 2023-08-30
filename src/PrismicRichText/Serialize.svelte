7855cdf560f35b6558286a52d5ef3e6a51fa49ca
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-08-30 11:31:22 -1000

    fix(PrismicRichText): prevent extra whitespace between text

--- src/PrismicRichText/Serialize.svelte
+++ src/PrismicRichText/Serialize.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import type { RichTextNodeType } from "@prismicio/client";
 	import type { asTree } from "@prismicio/richtext";
 
 	import type { SvelteRichTextSerializer } from "../types";
@@ -9,28 +8,29 @@
 	export let components: SvelteRichTextSerializer = {};
 	export let children: ReturnType<typeof asTree>["children"];
 
-	const rewrittenNodeTypes: Partial<
-		Record<
-			(typeof RichTextNodeType)[keyof typeof RichTextNodeType],
-			keyof typeof RichTextNodeType
-		>
-	> = {
+	const CHILD_TYPE_RENAMES = {
 		"list-item": "listItem",
 		"o-list-item": "oListItem",
 		"group-list-item": "list",
 		"group-o-list-item": "oList",
-	};
+	} as const;
+
+	function getComponent(child: ReturnType<typeof asTree>["children"][number]) {
+		return (
+			components[
+				CHILD_TYPE_RENAMES[child.type as keyof typeof CHILD_TYPE_RENAMES] ||
+					(child.type as keyof typeof components)
+			] || DefaultComponent
+		);
+	}
 </script>
 
 {#each children as child}
-	<svelte:component
-		this={components[rewrittenNodeTypes[child.type] || child.type] ||
-			DefaultComponent}
-		node={child.node}
-		key={child.key}
+	<svelte:component this={getComponent(child)} node={child.node}>
+		<!-- This formatting is intentional to prevent unwanted whitespace between elements. -->
+		{#if child.children.length > 0}<svelte:self
+				children={child.children}
+				{components}
+			/>{/if}</svelte:component
 	>
-		{#if child.children.length > 0}
-			<svelte:self children={child.children} {components} />
-		{/if}
-	</svelte:component>
 {/each}
