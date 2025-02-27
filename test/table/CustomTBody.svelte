499fa801cc4e2fa0ab43a2c6b2291be2fb36318c
Levi Mykel Gable <9385378+levimykel@users.noreply.github.com>
2025-02-27 17:59:53 +0100

    test: add tests for PrismicTable

---
+++ test/table/CustomTBody.svelte
@@ -0,0 +1,3 @@
+<!-- This file is a custom tbody component to be used in tests. -->
+
+<div class="body"><slot /></div>
---
+++ test/table/CustomTH.svelte
@@ -0,0 +1,3 @@
+<!-- This file is a custom th component to be used in tests. -->
+
+<div class="header"><slot /></div>
