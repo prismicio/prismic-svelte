5b8919278aa7bb47e194efd3877cf24068f2b482
Levi Mykel Gable <9385378+levimykel@users.noreply.github.com>
2025-02-28 13:06:06 +0100

    refactor: simplify default components

--- src/PrismicTable/DefaultComponent.svelte
+++ src/PrismicTable/DefaultComponent.svelte
@@ -1,19 +1,6 @@
 <script lang="ts">
-	import type { RTAnyNode } from "@prismicio/client";
-
-	export let type: "table" | "thead" | "tbody" | "tr" | "th" | "td";
+	import { TableComponents } from "../types";
+	export let type: keyof TableComponents;
 </script>
 
-{#if type === "table"}
-	<table><slot /></table>
-{:else if type === "thead"}
-	<thead><slot /></thead>
-{:else if type === "tbody"}
-	<tbody><slot /></tbody>
-{:else if type === "tr"}
-	<tr><slot /></tr>
-{:else if type === "th"}
-	<th><slot /></th>
-{:else if type === "td"}
-	<td><slot /></td>
-{/if}
+<svelte:element this={type}><slot /></svelte:element>
