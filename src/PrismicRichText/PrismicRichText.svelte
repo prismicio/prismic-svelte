b6e7f47759c15011dcf10f44d5cb50fc254b9e5f
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2025-12-18 11:16:16 -1000

    fix: revert PrismicRichText changes

--- src/PrismicRichText/PrismicRichText.svelte
+++ src/PrismicRichText/PrismicRichText.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
-	import type { asTree } from "@prismicio/client/richtext";
+	import type { RichTextField } from "@prismicio/client";
+	import { asTree } from "@prismicio/client/richtext";
 
 	import type { RichTextComponents } from "../types";
 
 	import Serialize from "./Serialize.svelte";
 
 	type Props = {
-		components: RichTextComponents;
-		children: ReturnType<typeof asTree>["children"];
+		/**
+		 * The Prismic rich text field to render.
+		 */
+		field: RichTextField;
+
+		/**
+		 * An object that maps a rich text block type to a Svelte component.
+		 */
+		components?: RichTextComponents;
 	};
 
-	const { components, children }: Props = $props();
+	const { field, components = {} }: Props = $props();
+
+	const children = $derived(asTree(field).children);
 </script>
+
+<!--
+  @component
+  Component to render a Prismic rich text field.
+
+  @example Rendering a rich text field:
+	```svelte
+		<PrismicRichText field={document.data.example_rich_text} />
+	```
+-->
 
 <Serialize {children} {components} />
